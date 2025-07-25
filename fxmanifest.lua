6d37870627e015e8a98c9fb69073c086f110757f
SecretEnemy <nodbyahhurd@gmail.com>
2025-07-25 17:10:45 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'Discord Role-Based Vehicle Whitelist (Auto Delete)'
-author 'SecretEnemy'
+author 'BigNolimits'
 
 server_script 'config.lua'
 server_script 'server.lua'
